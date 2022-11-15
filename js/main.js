1067041d4bac641105ce07252952666b32c52be7
Nikoloz Shervashidze <nikoshervashidze@gmail.com>
2022-11-16 01:32:37 +0400

    add modal final

--- js/main.js
+++ js/main.js
@@ -2,16 +2,14 @@ import { cardsData } from "./cards-data.js";
 
 const cardsContainer = document.getElementById("services-cards");
 const modal = document.getElementById("modal");
-
-const cardElement = document.querySelectorAll(".services-card");
-
-console.log(cardElement);
+const modalWrapper = document.getElementById("dialog-wrapper");
+const body = document.querySelector("body");
 
 function render() {
 	let feedHtml = "";
 
 	cardsData.forEach((card) => {
-		feedHtml += `<div class="services-card">
+		feedHtml += `<div class="services-card" id="services-card">
         <img src="${card.image}" alt="" />
         <p class="card-title">${card.title}</p>
         <p class="card-text">
@@ -25,12 +23,41 @@ function render() {
 
 cardsContainer.innerHTML = render();
 
-cardsContainer.addEventListener("click", (e) => {
-	modal.showModal();
+const cardElement = document.querySelectorAll("#services-card");
+console.log(cardElement);
+
+cardElement.forEach((element) => {
+	element.addEventListener("click", (e) => {
+		// body.style.overflow = "hidden";
+		modal.showModal();
+		function renderModalContent() {
+			let modalHtml = "";
+
+			cardsData.forEach((card) => {
+				let targetElement =
+					e.target.parentNode.children[1].innerText.toLocaleLowerCase();
+
+				if (card.title.toLocaleLowerCase() === targetElement) {
+					modalHtml = `<h1>Want to book our services?</h1>
+							<img src="${card.image}" alt="" />
+							<p class="card-title">${card.title}</p>
+							<p class="card-text">
+								${card.text}
+							</p>
+							<button class="modal-btn">Book Now!</button>`;
+				}
+			});
+
+			return modalHtml;
+		}
+
+		modalWrapper.innerHTML = renderModalContent();
+	});
 });
 
 modal.addEventListener("click", (e) => {
-	if (e.target == modal) {
+	if (e.target.id === "modal") {
 		modal.close();
+		// body.style.overflow = "auto";
 	}
 });
